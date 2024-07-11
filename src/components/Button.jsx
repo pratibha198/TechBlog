401469962fa6341ad91161c46faa5f07b37c29f3
pratibha kakodiya <122400036+pratibha198@users.noreply.github.com>
2024-07-11 18:15:54 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({
     children,
     type = "button",
-    bgColor = "bg-blue-600",
+    bgColor = "bg-rose-400",
     textColor = "text-white",
     className = "",
     ...props
