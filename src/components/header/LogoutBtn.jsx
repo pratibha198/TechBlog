5123301a8c21ded89afb4bc9eb862bc857162877
pratibha kakodiya <122400036+pratibha198@users.noreply.github.com>
2024-07-11 18:15:06 +0530

    Update LogoutBtn.jsx

--- src/components/header/LogoutBtn.jsx
+++ src/components/header/LogoutBtn.jsx
@@ -10,7 +10,7 @@ function LogoutBtn() {
         })
     }
   return (
-    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-white rounded-full'
     onClick={logoutHandler}
     >Logout</button>
   )
