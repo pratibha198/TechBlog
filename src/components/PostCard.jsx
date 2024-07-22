efa05bf2e66b2ad8e3edddd9fdbec8c9d680254a
pratibha kakodiya <122400036+pratibha198@users.noreply.github.com>
2024-07-23 00:57:54 +0530

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import appwriteService from "../appwrite/config"
 import {Link} from 'react-router-dom'
-
+import '../cardAni.css'
 function PostCard({$id, title, featuredImage}) {
     
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-white rounded-xl p-4'>
+        <div className='card w-full bg-white rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
                 <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
                 className='rounded-xl' />
